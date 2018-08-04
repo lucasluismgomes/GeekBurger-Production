73407bac7357e2aa7883415c0f15cbc34fd5a676
Lucas Gomes <Lucas Gomes>
2018-08-04 10:29:50 -0300

    Improv -> Implementação inicial mensageria

---
+++ GeekBurger.Production/GeekBurger.Production/Service/IProductionAreaChangedService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Production.Service
+{
+    public interface IProductionAreaChangedService
+    {
+        void SendMessagesAsync();
+        //void AddToMessageList(IEnumerable<EntityEntry<ProductionArea>> changes);
+    }
+}
---
+++ GeekBurger.Production/GeekBurger.Production/Service/ProductionAreaChangedService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Production.Service
+{
+    public class ProductionAreaChangedService : IProductionAreaChangedService
+    {
+        public void SendMessagesAsync()
+        {
+            
+        }
+    }
+}
