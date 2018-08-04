57303b4bd94ce637f6ef1e30e32dd0f9c8b7b163
Guilherme <36791418+guisdorneles@users.noreply.github.com>
2018-08-04 10:59:29 -0300

    Criação Contexto

---
+++ GeekBurger.Production/GeekBurger.Production/Repository/IProductionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Production.Repository
+{
+    public interface IProductionRepository
+    {
+        List<Production> ListProductions();
+        Production GetProductionById(Guid id);
+    }
+}
---
+++ GeekBurger.Production/GeekBurger.Production/Repository/ProductionRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeekBurger.Production.Repository
+{
+    public class ProductionRepository : IProductionRepository
+    {
+        private readonly ProductionContext _productionContext;
+        public ProductionRepository(ProductionContext context)
+        {
+            _productionContext = context;
+        }
+
+        public Production GetProductionById(Guid id)
+        {
+            return _productionContext.Production.Where(Z => Z.ProductionId == id).FirstOrDefault();
+        }
+
+        public List<Production> ListProductions()
+        {
+            return _productionContext.Production.ToList();
+        }
+    }
+}
