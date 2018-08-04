6bd34a2eb28bae815bc2a242da063206fda98f9a
Guilherme <36791418+guisdorneles@users.noreply.github.com>
2018-08-04 10:27:56 -0300

    Criação dbContext

---
+++ GeekBurger.Production/GeekBurger.Production/Model/Production.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Production
+{
+    public class Production
+    {
+        [Key]
+        public Guid ProductionId { get; set; }
+        public List<string> Restrictions { get; set; }
+        public bool On { get; set; }
+    }
+}
---
+++ GeekBurger.Production/GeekBurger.Production/Repository/ProductionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeekBurger.Production;
+
+namespace GeekBurger.Production.Repository
+{
+    public class ProductionContext : DbContext
+    {
+        public ProductionContext(DbContextOptions<ProductionContext> options) : base(options) { }
+            
+        public DbSet<Production> Production { get; set; }
+
+    }
+}
