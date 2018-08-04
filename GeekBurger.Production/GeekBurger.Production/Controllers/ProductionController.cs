802dcc7ab458acb955ac35f416fd9bfb38b27c89
Lucas Gomes <Lucas Gomes>
2018-08-04 09:54:23 -0300

    Fix -> Alteração do controller de listagem.

--- GeekBurger.Production/GeekBurger.Production/Controllers/ProductionController.cs
+++ GeekBurger.Production/GeekBurger.Production/Controllers/ProductionController.cs
@@ -2,55 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using GeekBurger.Production.Contract.Model;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekBurger.Production.Controllers
 {
-    [Route("api/production")]
+    [Route("api/production"), Produces("application/json")]
     public class ProductionController : Controller
     {
-        List<ProductionArea> ProductionAreas = new List<ProductionArea>();
-
-        public ProductionController()
-        {
-            ProductionAreas.AddRange(
-                new List<ProductionArea>
-                {
-                    new ProductionArea
-                    {
-                        ProductionId = new Guid("90f64d70-52bd-4039-bd26-454d94aff2e7"),
-                        Restrictions = new List<string>(){ "soy","dairy","gluten","peanut" },
-                        On = false
-                    },
-                    new ProductionArea
-                    {
-                        ProductionId = new Guid("6fa5817f-14db-4b2e-8591-490eb32c1495"),
-                        Restrictions = new List<string>(){ "dairy", "peanut" },
-                        On = false
-                    }
-                }
-            );
-        }
-
-        [HttpGet("areas")]
-        public IActionResult GetAreas()
-        {
-            return Ok(ProductionAreas);
-        }
-
-        [HttpGet("areas/{productionid}")]
-        public IActionResult GetAreaByProductionId(Guid productionId)
-        {
-            var productionArea = ProductionAreas
-                .FirstOrDefault(x => x.ProductionId == productionId);
-
-            if (productionArea == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(productionArea);
-        }
     }
 }
